f53095d673897cdca5fdf5e541d16b75a2c4fd37
KerenLart <kklartey@st.ug.edu.gh>
2025-08-09 11:16:38 +0000

    feature/added Abstract classes and Methods console app

---
+++ Abstract_Classes_and_Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace AbstractClassesAndMethods
+{
+    abstract class Shape
+    {
+        public abstract double GetArea();
+
+    }
+
+    class Circle : Shape
+    { 
+        public double Radius { get; set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+
+        }
+
+        public override double GetArea() 
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+
+    class Rectangle : Shape
+    {
+        public double Width { get; set; }
+        public double Height { get; set; }
+
+        public Rectangle(double width, double height)
+        {
+            Width = width;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Width * Height;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape circle =  new Circle(10);
+            Shape rectangle = new Rectangle(4, 7);
+
+            Console.WriteLine($"Circle area: {circle.GetArea()}");
+            Console.WriteLine($"Rectangle area: {rectangle.GetArea()}");
+
+        }
+    }
+
+}
